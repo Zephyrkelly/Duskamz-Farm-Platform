e5f3b34759cfde3ac04b58bdcbccad1825044041
Zephyrkelly <wkamaramadusu@gmail.com>
2026-07-31 23:13:12 +0100

    Point index.php to homepage.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-// Include database connection
 include("db_connect.php");
 
-// Simple homepage output
-echo "<h1>Welcome to Duskamz Farm Platform</h1>";
-echo "<p>Your PHP app is running on Render and connected to the database.</p>";
+// Include your actual homepage file
+include("homepage.php");
 ?>
